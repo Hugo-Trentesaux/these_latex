19874f8e3cfb4ff5b5aff1e043511a24f1b9b088
Hugo Trentesaux <hugo@trentesaux.fr>
2020-09-25 10:52:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+Ceci est ma thèse en LaTeX utilisant le template fourni par Jean Hare (version 2017).
+
+## Installation
+
+paquet ArchLinux:
+- texlive-core
+- texlive-fontsextra
+- tllocalmgr ou fix pour tlmgr
+
+paquets CTAN
+- bbm
+- upgreek (via was)
+- datetime
+- fmtcount
+- slantsc
+- titlesec
+- blindtext
+- changebar
+- versions
+- adjustbox
+- xstring
+- csquotes
+
+## Bibliographie
+
+Échapper manuellement les `%` en les remplaçant par des `\%`.
---
+++ scripts/compresspdf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Clean, sanitize and compress a list of PDF files
+for fullfile in "$@"
+do
+   filenameext=$(basename "$fullfile") 
+   extension="${filenameext##*.}"
+   filename="${filenameext%.*}"
+   echo fix $filenameext with PDFtk and GS
+   pdftk "$filenameext" output "$filename"_tk.pdf&& \
+      gs -q -dSAFER -dNOPAUSE -dBATCH -sDEVICE=pdfwrite  -dCompatibilityLevel=1.5 -r300 -dPDFSETTINGS=/ebook -sOUTPUTFILE="$filename"_C.pdf -f "$filename"_tk.pdf&& \
+      /bin/rm -f "$filename"_tk.pdf
+      retval=$?
+      if [ $retval = 0 ]; then
+         echo "   done"
+      fi
+      ls -l "$filename"*.pdf | awk '{printf("%-6s %-35s\t  %7d\n", " ",$9, $5); }'
+done
